f76ad4087dcc8890b1a95ac1e2938ffb6f9fe1c5
mtx926 <17756534008@163.com>
2024-10-29 16:03:04 +0800

    新增员工业务代码开发

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -11,13 +11,16 @@ public interface EmployeeMapper {
 
     /**
      * 根据用户名查询员工
-     *
      * @param username
      * @return
      */
     @Select("select * from employee where username = #{username}")
     Employee getByUsername(String username);
 
+    /**
+     * 新增员工
+     * @param employee
+     */
     @Insert("INSERT INTO employee (name, username, password, phone, sex, id_number, status,create_time, update_time, create_user, update_user) " +
             "VALUES"+
             " (#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{status},#{createTime},#{updateTime},#{createUser},#{updateUser})")
